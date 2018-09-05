3e39f4cce33100a1c812a13a1684b312ecf16b16
Emmanuel Hesry <ehesry@gmail.com>
2018-09-05 15:44:30 +0200

    Migrate to webpack 3

--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,20 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
         exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015', 'stage-1']
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['react', 'es2015', 'stage-1']
+            }
         }
       }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
